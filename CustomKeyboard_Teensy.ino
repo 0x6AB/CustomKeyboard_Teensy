b8a6d12651242af0140f2303845975eaf412e756
0x6AB <73175542+0x6AB@users.noreply.github.com>
2020-10-20 16:18:42 +0300

    Initial commit

---
+++ CustomKeyboard_Teensy.ino
@@ -0,0 +1,79 @@
+void setup() {
+  randomSeed(analogRead(0));
+  Serial.begin(115200); // устанавливаем последовательное соединение
+}
+
+int i=0;
+byte data[1024];
+
+struct base_data
+{
+   byte type;
+   word timing;
+   byte floating_border;
+   byte arg[512];
+};
+typedef struct base_data Base_data;
+
+void loop() {
+  i=0;
+  if (Serial.available() > 0) 
+  { 
+    memset(data, 0, 511);
+    while(Serial.available() > 0) {
+      data[i] = Serial.read();
+      i++;
+    }
+    Base_data *sdata = (Base_data*)data; 
+    
+    
+
+    if(sdata->type == 0) {
+      Serial.print("type = ");
+      Serial.println(sdata->type);
+  
+      Serial.print("timing = ");
+      Serial.println(sdata->timing);
+  
+      Serial.print("floating_border = ");
+      Serial.println(sdata->floating_border);
+    }
+    
+    if(sdata->type == 1) {
+      Serial.println((char*)sdata->arg);
+      Keyboard.print((char*)sdata->arg);
+    }
+
+    if(sdata->type == 2) {
+      Keyboard.press((sdata->arg)[0]);
+      delay(sdata->timing);
+      Keyboard.release((sdata->arg)[0]);
+    }
+    
+    if(sdata->type == 3) {
+      int i = 0;
+      while( sdata->arg[i]!=0 ) {
+        Keyboard.press((sdata->arg)[i]);
+        delay(sdata->timing+random(sdata->floating_border));
+        Keyboard.release((sdata->arg)[i]);
+        delay(sdata->timing+random(sdata->floating_border));
+        i++;
+      }
+    }
+
+    if(sdata->type == 4) {
+      int i = 0;
+      while( sdata->arg[i]!=0 ) {
+        Keyboard.press((sdata->arg)[i]);
+        delay(sdata->timing+random(sdata->floating_border));
+        Keyboard.release((sdata->arg)[i]);
+        delay(sdata->timing+random(sdata->floating_border));
+        i++;
+        Serial.println("w");
+      }
+      Serial.println("e");
+    }
+    
+    Serial.flush();
+  }
+}
